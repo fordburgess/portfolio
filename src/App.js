b0f14c9b1e1a16d9bb806ca35436eab116aee4f4
Ford Burgess <ford.burgess@abintus.consulting>
2024-03-25 15:49:07 +0000

    added gradient

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import Home from './features/Home';
 import './App.css';
+import { Gradient } from './components/Gradient.js'
 
 function App() {  
 
+  useEffect(() => {
+    const gradient = new Gradient();
+    gradient.initGradient('#gradient-canvas');
+  }, [])
+
   return (
-    <div className="App">       
+    <div className="App">    
+      <canvas id="gradient-canvas" data-transition-in />
       <Home />      
     </div>
   );
