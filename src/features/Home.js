a5ac0c0971e8eb88b43beaf5874a495ddb29a69a
Ford Burgess <ford.burgess@abintus.consulting>
2024-03-26 10:43:56 +0000

    left transition complete

--- src/features/Home.js
+++ src/features/Home.js
@@ -1,16 +1,23 @@
 import React, { useEffect } from 'react'
+import './styles/Home.css'
 
 const Home = () => {    
 
   useEffect(() => {
-    const title = document.getElementById('title');
-    const subtitle = document.getElementById('subtitle')
+    const title = document.getElementById('home-title');
+    const subtitle = document.getElementById('home-subtitle')
 
     const resize = () => {
         const scrollPos = window.scrollY;
 
         title.style.fontSize = `clamp(2.5rem, ${6 - (scrollPos * 0.05)}rem, 6rem)`;
-        subtitle.style.fontSize = `clamp(1rem, ${2 - (scrollPos * 0.045)}rem, 2rem)`;        
+        subtitle.style.fontSize = `clamp(1rem, ${2 - (scrollPos * 0.045)}rem, 2rem)`;    
+
+        var leftValTitle = 30 - (scrollPos * 0.5) > 0 ? 30 - (scrollPos * 0.5) : 0;
+        var leftValSubtitle = 40 - (scrollPos * 0.5) > 0 ? 40 - (scrollPos * 0.5) : 0;
+
+        title.style.left = `${leftValTitle}%`;        
+        subtitle.style.left = `${leftValSubtitle}%`;       
     }
 
     window.addEventListener('scroll', resize)
@@ -22,16 +29,11 @@ const Home = () => {
 
 
   return (
-    <div style={{ height: '200vh' }}>
-        <div className='w-full h-screen p-20 flex'>
-            <div className='w-full h-full text-center flex flex-col justify-center'>
-                <div>
-                    <h1 className='header' id="title" style={{ fontSize: '6rem'}}>Ford Burgess</h1>
-                    <h3 className='subtitle' id="subtitle" style={{ fontSize: '2rem'}}>Full Stack Developer</h3>                    
-                    {/* <p className='description'>
-                        I love my gf!!
-                    </p> */}
-                </div>                
+    <div style={{ height: '200vh', boxSizing: 'border-box' }}>
+        <div className='w-full h-screen py-4 px-6'>
+            <div className='w-full h-full relative flex flex-col'>                
+                <h1 className='header' id="home-title" style={{ fontSize: '6rem'}}>Ford Burgess</h1>
+                <h3 className='subtitle' id="home-subtitle" style={{ fontSize: '2rem'}}>Full Stack Developer</h3>                           
             </div>         
         </div>
     </div>
