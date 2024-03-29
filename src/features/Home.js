fc1c1d704ef5489964a248cb05cecbd5cae0e8ad
Ford Burgess <ford.burgess@abintus.consulting>
2024-03-29 15:56:15 +0000

    wrote first drafts

--- src/features/Home.js
+++ src/features/Home.js
@@ -62,13 +62,13 @@ const Home = () => {
                 <h1 className='header' id="home-title" style={{ fontSize: '6rem'}}>Ford Burgess</h1>
                 <h3 className='subtitle' id="home-subtitle" style={{ fontSize: '2rem'}}>Full Stack Developer</h3> 
                 <div id="description-and-nav">
-                    <p className='description mb-8' id="home-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in.</p> 
+                    <p className='description mb-8' id="home-description">I combine my affinity for design and passion for complex, technical projects to build applications that are appealing, accessible, and most of all, helpful and enjoyable to use.</p> 
                     <ul>
                         <li className="marker" id="professional-marker">
-                            <h4 className=''>Professional</h4>
+                            <h4 className='font-montserrat'>Professional</h4>
                         </li>
                         <li className="marker" id="freelance-marker">
-                            <h4 className=''>Freelance</h4>
+                            <h4 className='font-montserrat'>Freelance</h4>
                         </li>
                     </ul>
                 </div>
@@ -82,12 +82,12 @@ const Home = () => {
                             <div key={index} className='w-3/4 flex flex-col justify-start items-start mb-16' id="experience-section">
                                 <h3 className='subtitle font-black'><strong>{item.name}</strong></h3>
                                 <p className='description text-xl mb-3'>{item.dates}</p>
-                                <p className='description text-sm mb-3'>{item.description}</p>
+                                <div className='description text-sm mb-3' dangerouslySetInnerHTML={{__html: item.description}}></div>
                                 <div  className='w-full flex flex-wrap'>
                                     {
                                         item.stack.map((tech, i) => {
                                             return (
-                                                <div key={i} className='px-2 bg-white bg-opacity-70 rounded-xl m-0.5'>
+                                                <div key={i} className='px-2 py-0.5 bg-white bg-opacity-70 rounded-xl m-1'>
                                                     <p className='description m-0 text-xs text-black'>{tech}</p>
                                                 </div>
                                             )
@@ -97,16 +97,25 @@ const Home = () => {
                             </div>
                         )
                     })
-                }
-                {/* <h3 className='subtitle text-5xl pl-4'>Professional</h3> */}               
-                {/* <h3 className='subtitle text-5xl pl-4'>Freelance and Personal</h3> */}
+                }                
                 {
                     experience.experience.freelance.map((item, index) => {
                         return (
                             <div className='w-3/4 flex flex-col justify-start items-start mb-16' id="experience-section">
                                 <h3 className='subtitle font-black'><strong>{item.name}</strong></h3>
-                                <p className='description text-xl mb-4'>{item.dates}</p>
-                                <p className='description text-sm'>{item.description}</p>
+                                <p className='description text-xl mb-3'>{item.dates}</p>                                
+                                <div className='description text-sm mb-3' dangerouslySetInnerHTML={{__html: item.description}}></div>
+                                <div  className='w-full flex flex-wrap'>
+                                    {
+                                        item.stack.map((tech, i) => {
+                                            return (
+                                                <div key={i} className='px-2 py-0.5 bg-white bg-opacity-70 rounded-xl m-1'>
+                                                    <p className='description m-0 text-xs text-black'>{tech}</p>
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
                             </div>
                         )
                     })
